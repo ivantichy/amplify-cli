175b6eba200eb179f3ca7bb87399b9ff38001a34
John Corser <xss@amazon.com>
2022-05-11 16:26:44 +0000

    feat: allow login via admin ui on safari

--- packages/amplify-provider-awscloudformation/src/admin-login.ts
+++ packages/amplify-provider-awscloudformation/src/admin-login.ts
@@ -1,40 +1,57 @@
-import ora from 'ora';
 import { $TSContext, open } from 'amplify-cli-core';
-
-import { adminVerifyUrl, adminBackendMap, isAmplifyAdminApp } from './utils/admin-helpers';
+import { prompter } from 'amplify-prompts';
+import { adminVerifyUrl, adminBackendMap, isAmplifyAdminApp } from './utils/admin-helpers';  // eslint-disable-line
 import { AdminLoginServer } from './utils/admin-login-server';
+import { AdminAuthPayload } from './utils/auth-types';
 
-export async function adminLoginFlow(context: $TSContext, appId: string, envName?: string, region?: string) {
-  envName = envName || context.amplify.getEnvInfo().envName;
+/**
+ * Launches browser and promises to receive authentication from browser
+ */
+export const adminLoginFlow = async (context: $TSContext, appId: string, envName?: string, region?: string): Promise<void> => {
+  envName = envName || context.amplify.getEnvInfo().envName;  // eslint-disable-line
   if (!region) {
     const { isAdminApp, region: _region } = await isAmplifyAdminApp(appId);
     if (!isAdminApp) {
       throw new Error(`Amplify Studio not enabled for appId: ${appId}`);
     }
-    region = _region;
+    region = _region; // eslint-disable-line
   }
 
   const url = adminVerifyUrl(appId, envName, region);
   context.print.info(`Opening link: ${url}`);
   await open(url, { wait: false }).catch(e => {
     context.print.error(`Failed to open web browser: ${e.message || e}`);
-    return;
   });
-  const spinner = ora('Continue in browser to log in…\n').start();
+  context.print.info('Continue in browser to log in…');
   try {
     // spawn express server locally to get credentials
     // environment variable AMPLIFY_CLI_ADMINUI_BASE_URL is used to set the login url to http://localhost:3000 when developing against beta/gamma endpoints
     const originUrl = process.env.AMPLIFY_CLI_ADMINUI_BASE_URL ?? adminBackendMap[region]?.amplifyAdminUrl;
     const adminLoginServer = new AdminLoginServer(appId, originUrl, context.print);
-    await new Promise<void>(resolve =>
+    const getTokenViaServer = new Promise<void>(resolve => {
       adminLoginServer.startServer(() => {
         adminLoginServer.shutdown();
-        spinner.succeed('Successfully received Amplify Studio tokens.');
+        context.print.success('Successfully received Amplify Studio tokens.');
         resolve();
-      }),
-    );
+      });
+    });
+
+    const getTokenViaPrompt = new Promise<void>(resolve => {
+      prompter.input('Paste token from browser to log in:').then(async (tokenBase64: string) => {
+        try {
+          const tokenJson = JSON.parse(Buffer.from(tokenBase64, 'base64').toString()) as unknown as AdminAuthPayload;
+          await adminLoginServer.storeTokens(tokenJson, appId);
+        } catch (e) {
+          console.log('Provided token was invalid.');
+          throw e;
+        }
+        context.print.success('Successfully received Amplify Studio tokens.');
+        resolve();
+      });
+    });
+
+    await Promise.race([getTokenViaPrompt, getTokenViaServer]);
   } catch (e) {
-    spinner.stop();
     context.print.error(`Failed to authenticate with Amplify Studio: ${e.message || e}`);
   }
-}
+};
--- packages/amplify-provider-awscloudformation/src/utils/admin-login-server.ts
+++ packages/amplify-provider-awscloudformation/src/utils/admin-login-server.ts
@@ -1,8 +1,8 @@
 import { $TSContext, stateManager } from 'amplify-cli-core';
 import { CognitoIdentity } from 'aws-sdk';
-import bodyParser from 'body-parser';
+import bodyParser from 'body-parser';  // eslint-disable-line
 import cors from 'cors';
-import express from 'express';
+import express from 'express';  // eslint-disable-line
 import http from 'http';
 import * as jose from 'jose';
 import _ from 'lodash';
@@ -10,6 +10,9 @@ import * as assert from 'assert';
 
 import { AdminAuthPayload, CognitoIdToken, CognitoAccessToken } from './auth-types';
 
+/**
+ * Admin login server class
+ */
 export class AdminLoginServer {
   private app: express.Application;
   private appId: string;
@@ -28,7 +31,7 @@ export class AdminLoginServer {
     this.appId = appId;
     this.corsOptions = {
       origin: [originUrl],
-      methods: ['POST', 'OPTIONS'],
+      methods: ['GET', 'POST', 'OPTIONS'],
       allowedHeaders: 'Content-Type',
     };
     this.print = print;
@@ -37,22 +40,25 @@ export class AdminLoginServer {
     this.app.use(bodyParser.json());
   }
 
-  public async startServer(callback: () => void) {
+  /**
+   * Sets up express server to listen for tokens
+   */
+  public async startServer(callback: () => void): Promise<void> {
     await this.setupRoute(callback);
     // Need to specify hostname for WSL
     this.server = this.app.listen(this.getPort(), this.getHost());
   }
 
-  private getHost() {
+  private getHost(): string {
     return this.host;
   }
 
   // TODO: scan for available ports across a range like mock
-  private getPort() {
+  private getPort(): number {
     return this.port;
   }
 
-  private async getIdentityId(idToken: CognitoIdToken, IdentityPoolId: string, region: string): Promise<string> {
+  private async getIdentityId(idToken: CognitoIdToken, IdentityPoolId: string, region: string): Promise<string> { // eslint-disable-line
     const cognitoIdentity = new CognitoIdentity({ region });
     const login = idToken.payload.iss.replace('https://', '');
     const logins = {
@@ -70,12 +76,12 @@ export class AdminLoginServer {
     return IdentityId;
   }
 
-  private async setupRoute(callback) {
-    this.app.post('/amplifyadmin/', async (req, res) => {
+  private async setupRoute(callback): Promise<void> {
+    this.app.post('/amplifyadmin/', async (req, res) => { // eslint-disable-line
       if (!req.body || req.body.error) {
         this.shutdown();
         if (req.body.error === 'CANCELLED') {
-          this.print.info('Login cancelled');
+          this.print.info('Login canceled');
           process.exit(0);
         }
         throw new Error('Failed to receive expected authentication tokens.');
@@ -90,20 +96,24 @@ export class AdminLoginServer {
       }
       callback();
     });
+    this.app.get('/ping', async (_, res) => { // eslint-disable-line
+      res.send({ success: true });
+      callback();
+    });
   }
 
-  private async validateTokens(
+  private async validateTokens( // eslint-disable-line
     tokens: {
       accessToken: CognitoAccessToken;
       idToken: CognitoIdToken;
     },
-  ) {
+  ): Promise<boolean> {
     const issuer: string = tokens.idToken.payload.iss;
     const audience: string = tokens.idToken.payload.aud;
 
     const N_JWKS = jose.createRemoteJWKSet(new URL(`${issuer}/.well-known/jwks.json`));
 
-    const { payload: decodedJwtId } = await jose.jwtVerify(tokens.idToken.jwtToken, N_JWKS, { issuer, audience })
+    const { payload: decodedJwtId } = await jose.jwtVerify(tokens.idToken.jwtToken, N_JWKS, { issuer, audience });
     if (Array.isArray(decodedJwtId.aud) && decodedJwtId.aud.length > 1) {
       assert.strictEqual(decodedJwtId.azp, audience); // checking mandatory presence as per the ID Token profile
     }
@@ -111,17 +121,20 @@ export class AdminLoginServer {
     assert.ok('iat' in decodedJwtId); // checking mandatory presence
     assert.ok('exp' in decodedJwtId); // checking mandatory presence, when present it was already validated
 
-    const { payload: decodedJwtAccess } = await jose.jwtVerify(tokens.accessToken.jwtToken, N_JWKS)
+    const { payload: decodedJwtAccess } = await jose.jwtVerify(tokens.accessToken.jwtToken, N_JWKS);
 
     return _.isEqual(decodedJwtId, tokens.idToken.payload) && _.isEqual(decodedJwtAccess, tokens.accessToken.payload);
   }
 
-  private async storeTokens(payload: AdminAuthPayload, appId: string) {
+  /**
+   * Stores tokens received by server
+   */
+  public async storeTokens(payload: AdminAuthPayload, appId: string): Promise<void> {
     const areTokensValid = await this.validateTokens(
       {
         idToken: payload.idToken,
         accessToken: payload.accessToken,
-      }
+      },
     );
     if (areTokensValid) {
       const IdentityId = await this.getIdentityId(payload.idToken, payload.IdentityPoolId, payload.region);
@@ -130,7 +143,10 @@ export class AdminLoginServer {
     }
   }
 
-  shutdown() {
+  /**
+   * Shuts down the server
+   */
+  shutdown(): void {
     this.server.close();
   }
 }
